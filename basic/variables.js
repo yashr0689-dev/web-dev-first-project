916ad61f092ac5b31380c335ae611cced8aee957
yashr0689-dev <yashr0689@gmail.com>
2026-01-30 07:47:10 +0000

    types of variables

---
+++ basic/variables.js
@@ -0,0 +1,17 @@
+const accountId = 12344;
+let accountUser = 'Yash Raj'
+let accountPassword = "1313*!2wwk"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountPassword, accountUser, accountState])
+// number => 2 to power 53
+// bigint
+// string =
+// boolean = true/false
+// null = standalone value
+// undefined =>
+// symbol → unique
+// object
+console. log(typeof undefined); // undefined
+console. log (typeof null); // object
