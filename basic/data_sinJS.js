71a2cf73bad25b50565c03f2d3efa0edbcb5a2cb
yashr0689-dev <yashr0689@gmail.com>
2026-02-08 08:50:32 +0000

    study of dates in js

--- basic/data_sinJS.js
+++ basic/data_sinJS.js
@@ -1,22 +1,30 @@
 // Dates
+const mysecdate = new Date()
+const mydate = Date()
+console.log(mydate);
+console.log(mysecdate);
+console.log(mysecdate.toString());
+console.log(mysecdate.toLocaleString());
+console.log(mysecdate.toDateString());
+
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate3 = new Date(2023, 0, 23)
+let myCreatedDate1 = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate2 = new Date("01-14-2023")
+console.log(myCreatedDate2);
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp);// time in millisec from 1st jan 1970s to now use in quizes and compitition to compare 
+console.log(myCreatedDate2.getTime()); // it is in millisecond
+console.log(Math.floor(Date.now()/1000)); // to convert in sec
 
 let newDate = new Date()
 console.log(newDate);
@@ -29,3 +37,4 @@ newDate.toLocaleString('default', {
     weekday: "long",
     
 })
+console.log(newDate);
